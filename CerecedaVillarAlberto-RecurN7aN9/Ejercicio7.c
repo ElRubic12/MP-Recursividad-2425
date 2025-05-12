4ed99f718dbb83d92552502f6c8b9bcacc447a67
ElRubic12 <alberto.cerevillar@alum.uca.es>
2025-05-12 15:17:27 +0200

    Cambios en el ejercicio 7

--- CerecedaVillarAlberto-RecurN7aN9/Ejercicio7.c
+++ CerecedaVillarAlberto-RecurN7aN9/Ejercicio7.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
-#include<string.h>
 #include<stdlib.h>
+#include<string.h>
 
-#define M 5
 #define N 5
-
-void funcionmatriz( int m[M][N], int x, int y, int i, int j, int cont, int filas, int colum );
+#define M 5
 
 int main()
 {
-    int i, j, x, y;
+    int i, j;
 
     int matriz[M][N] = {
         {4, 7, 2, 1, 9},
@@ -38,57 +36,5 @@ int main()
             printf( "%d   ", matriz[i][j] );    ///Espaciado uniforme
         }
         printf( "\n" );
-    }
-
-    do{                            ///Se introducen las coordenadas de la matriz
-        printf( "Introduce las coordenadas de una posicion de la matriz(0-%d):\n", M-1 );
-        printf( "Introduce la coordenada x: " );
-        scanf( "%d", &x );
-        printf( "Introduce la coordenada y: " );
-        scanf( "%d", &y );
-    }while( x < 0 || x >= M || y < 0 || y >= N );       ///Mientras sea menor que 0 y mayor que el tamaño de la matriz
-
-    printf( "Numero elegido: %d.\n", matriz[x][y] );
-
-    funcionmatriz( matriz, x, y, 0 , 0, 0, 0, 0 );
-
-    system( "pause" );
-
-}
-
-///Cabecera: int m[M][N], int x, int y, int i, int j, int cont, int filas, int colum
-///Precondicion: Recibe una matriz desordenada, las posiciones x e y elegidas, 2 iteradores i y j, un contador para contar
-///              las veces que recorre las filas y columnas, el numero de elementos de filas y columnas
-///Postcondicion: Devuelve el numero de elementos que son menores que los elementos de la fila y el numero de elementos que 
-///              son mayores que los elementos de la columna del numero elegido
-
-void funcionmatriz( int m[M][N], int x, int y, int i, int j, int cont, int filas, int colum )
-{
-    if( cont == M*2 )           ///Caso base cuando se han recorrido todos los elementos de las filas y columnas desde el numero elegido
-    {
-        printf( "El numero %d es mayor que %d numeros de su fila.\n", m[x][y], filas );
-        printf( "El numero %d es menor que %d numeros de su columna.\n", m[x][y], colum );
-    }else{
-
-        if( j < N )             ///Primero recorren los elementos de la fila desde el numero elegido
-        {
-            if( m[x][j] < m[x][y] )         ///Caso en que el elemento de la fila sea menor que el numero elegido aumenta la variable filas
-            {
-                return funcionmatriz( m, x, y, i, j+=1, cont+=1, filas+=1, colum );
-            }else{
-                return funcionmatriz( m, x, y, i, j+=1, cont+=1, filas, colum );
-            }
-        }
-
-        if( i < M )             ///Despues se recorren los elementos de la columna desde el numero elegido
-        {
-            if( m[i][y] > m[x][y] )         ///Caso en que el elemento de la columna sea mayor que el numero elegido aumenta la variable colum
-            {
-                return funcionmatriz( m, x, y, i+=1, j, cont+=1, filas, colum+=1 );
-            }else{
-                return funcionmatriz( m, x, y, i+=1, j, cont+=1, filas, colum );
-            }
-        }
-
     }
 }
